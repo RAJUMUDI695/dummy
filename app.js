3cc2ad91edaaaddcd53e0b2613c0c428a2b91ba0
RAJUMUDI695 <raju.mudi222@gmail.com>
2024-10-01 12:10:53 +0530

    commit

---
+++ app.js
@@ -0,0 +1,49 @@
+
+// count of fruits
+
+// const fruits = ["apple", "banana", "apple", "banana", "orange", "apple"];
+
+// const fruitCounts = fruits.reduce((acc, fruit) => {
+//   acc[fruit] = (acc[fruit] || 0) + 1;
+//   return acc;
+// }, {});
+
+// console.log(fruitCounts);
+
+
+
+// code for replacing name
+// const fruits = ["rama", "raja", "rama", "ram"];
+
+// const updatedFruits = fruits.map(fruit => {
+//   if (fruit === "rama") {
+//     return "lakshman";
+//   }
+//   return fruit;
+// });
+
+// console.log(updatedFruits);
+
+
+//code for giving array for repeated name
+// const fruits = ["rama", "raja", "rama", "ram"];
+
+// const repeatedFruits = fruits.filter(fruit => fruit === "rama");
+
+// console.log(repeatedFruits);
+
+
+
+// const myImage = document.getElementById("myImage");
+// const enlargeButton = document.getElementById("enlargeButton");
+
+// enlargeButton.addEventListener("click", () => {
+//   if (myImage.style.width === "400px") {
+//     myImage.style.width = "200px";
+//     myImage.style.height = "150px";
+//   } else {
+//     myImage.style.width = "400px";
+//     myImage.style.height = "300px";
+//   }
+// });
+
